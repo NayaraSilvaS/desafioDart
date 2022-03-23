3b09d2f4d303aef7b408f33afc0e00723cbfaa08
Nayara Silva <99221251+NayaraSilvaS@users.noreply.github.com>
2022-03-22 21:41:32 -0300

    Add files via upload

---
+++ desafio/desafio/Lista.dart
@@ -0,0 +1,12 @@
+void main(){
+  var list = [];
+  lista.add("NayNay");
+  lista.add("Caio-San");
+  lista.add("Filipinas");
+  lista.add("Sadnay");
+  lista.add("Gustavão");
+  lista.add("Arinelson");
+
+  lista.sort();
+  print(lista);
+}
---
+++ desafio/desafio/desafio.dart
@@ -0,0 +1,14 @@
+void main(){
+  int notaCleosvaldo = 70;
+  int notaJudicleison = 63;
+  int notaJobnilson = 89;
+  int notaJovercia = 92;
+  int notaCleoniza = 58;
+  int notaMarizinete = 57;
+  
+  int total = (notaCleosvaldo + notaJudicleison + notaJobnilson + notaJovercia + notaCleoniza + notaMarizinete);
+  
+  double media = total /6;
+  
+  print("a média total das notas é: $media");
+}
